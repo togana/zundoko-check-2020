fe21f154ceb724ccf1ce27fd425c32d08bb4c504
togana <togana@users.noreply.github.com>
2020-05-10 08:35:09 +0000

    deploy: d0964e08f95ea8c8b625d5ce6dadea0ad929b432

---
+++ precache-manifest.b939720e6efbcb837ccf1385c8f6ae0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60fab4b70aa23b746764b17d2be69193",
+    "url": "/zundoko-check-2020/index.html"
+  },
+  {
+    "revision": "59c77542199fdfa0c034",
+    "url": "/zundoko-check-2020/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "1dc574f218b96dcfc33f",
+    "url": "/zundoko-check-2020/static/js/2.00e2a329.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/zundoko-check-2020/static/js/2.00e2a329.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59c77542199fdfa0c034",
+    "url": "/zundoko-check-2020/static/js/main.04714abe.chunk.js"
+  },
+  {
+    "revision": "4aa3fcf56d1234311a0f",
+    "url": "/zundoko-check-2020/static/js/runtime-main.ed266b31.js"
+  }
+]);
